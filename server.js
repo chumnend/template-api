50f74cfdda069c1364c3f6e1ce9f43adfda321dd
Nicholas Chumney <nchumney@hotmail.com>
2019-12-07 22:04:16 +0000

    changed default port for server

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require("./app");
 const config = require("./config");
+const port = config.PORT || 3000;
 
 // start the server
-const port = config.PORT || 4000;
 app.listen(port, () => {
     console.log("Server started on port", port);
-})
+});

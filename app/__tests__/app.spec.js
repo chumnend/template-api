88b9ebbe24ec5ead32b60acf349b4f4bbcb6b315
Nicholas Chumney <nchumney@hotmail.com>
2019-12-21 21:00:06 +0000

    refactored testing structure

---
+++ app/__tests__/app.spec.js
@@ -0,0 +1,31 @@
+const chai = require("chai");
+const expect = chai.expect;
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+
+module.exports = function (app) 
+{
+    describe("/", function () {
+        it("expects to be ready to serve", function(done) {
+                chai.request(app)
+                .get("/")
+                .end( (err, res) => {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                done();
+            });
+        });
+    });
+    
+    describe("/not-a-path", function () {
+        it("expects to encounter 404", function(done) {
+            chai.request(app)
+            .get("/not-a-path")
+            .end( (err, res) => {
+               expect(err).to.be.null;
+               expect(res).to.have.status(404);
+               done();    
+            });
+        });
+    });
+};
--- test/setupTests.js
+++ test/setupTests.js
@@ -1,27 +1,4 @@
 const app = require("../app");
+const appTest = require("../app/__tests__/app.spec");
 
-const chai = require("chai");
-const expect = chai.expect;
-const chaiHttp = require("chai-http");
-chai.use(chaiHttp);
-
-describe("SERVER", function() {
-    it("expects to be ready to serve", function(done) {
-        chai.request(app)
-            .get("/")
-            .end( (err, res) => {
-               expect(err).to.be.null;
-               expect(res).to.have.status(200);
-               done();
-            });
-    });
-    it("expects to encounter 404", function(done) {
-        chai.request(app)
-            .get("/not-a-path")
-            .end( (err, res) => {
-               expect(err).to.be.null;
-               expect(res).to.have.status(404);
-               done();    
-            });
-    });
-});
+describe("APP TEST", appTest.bind(this, app));

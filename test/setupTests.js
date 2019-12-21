da18772d4ef8d849ca9a7dcc73df6adca6c13bc1
Nicholas Chumney <nchumney@hotmail.com>
2019-12-21 02:30:12 +0000

    added 404 test

--- test/setupTests.js
+++ test/setupTests.js
@@ -6,7 +6,7 @@ const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 
 describe("SERVER", function() {
-   it("expects to be ready to serve", function(done) {
+    it("expects to be ready to serve", function(done) {
         chai.request(app)
             .get("/")
             .end( (err, res) => {
@@ -14,5 +14,14 @@ describe("SERVER", function() {
                expect(res).to.have.status(200);
                done();
             });
-   });
+    });
+    it("expects to encounter 404", function(done) {
+        chai.request(app)
+            .get("/not-a-path")
+            .end( (err, res) => {
+               expect(err).to.be.null;
+               expect(res).to.have.status(404);
+               done();    
+            });
+    });
 });
